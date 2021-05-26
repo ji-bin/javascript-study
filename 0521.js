7a7fa263981b73612cf717593308cebf55084c20
ji-bin <yks04ever@naver.com>
2021-05-27 01:16:36 +0900

    복습

---
+++ 0521.js
@@ -0,0 +1,129 @@
+// 데이터 타입
+
+// 문자 ( string ) ""
+// 숫자 ( number ) 12.2 12412421 124135135151252
+// 언디파인드 ( undefined ) 
+// 널 ( null )
+// 불린 (true, false)
+// symbol ( 심볼 )
+
+// -------------------- //
+
+// object 배열 객체 함수
+
+// function 
+
+
+// function add(a,c){
+//     return (a+a)*c;
+// }
+
+// const result = add(2,4);
+// console.log(result);
+
+// // 사칙연산 함수 ( 덧셈, 뺼셈, 곱셈, 나눗셈 )
+// // 위 함수들을 한번에 실행시켜주는 함수
+// const a = 3;
+// const b = 4;
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function plus(a, b){
+//     return a+b;
+// }
+// function minus(a, b){
+//     return a-b;
+// }
+// function multi(a, b){
+//     return a*b;
+// }
+// function division(a, b){
+//     return a / b;
+// }
+
+
+// function init(i, j){
+//     const pluseValue = plus(a, b);
+//     const minusValue = minus(i, j);
+//     const multiValue = multi(i, j);
+//     const divisionValue = division(i, j);
+//     console.log(pluseValue, minusValue, multiValue, divisionValue);
+// }
+
+// init(1, 2)
+
+
+// ?님 안녕하세요 나이는 ?살 성별은 ?입니다 
+// function hi(name, age = 3, gender){
+//     console.log(name);
+//     console.log(age);
+//     console.log(gender);
+//     console.log(name + "님 안녕하세요. 나이는" + age + "살 성별은 " + gender + "입니다.");
+// }
+
+// // hi( "test", 0,  "남자");
+
+// console.log(minus(2,3));
+
+
+// function add(a,b){ // 함수선언 
+//     return a + b;
+// }
+
+// const minus = (a, b) => { // 함수표현
+
+//     return a - b;
+// }
+
+// if 조건문은 코드실행의 흐름을 조정하는 구문 
+
+
+// 90이상 A,, 90미만 80이상 B, 80 ~ 70, 70 ~ 60
+
+// function test(score){
+//     if(90 <= score){
+//         return "A"
+//     }else if(90 > score >= 80){
+//         return "B"
+//     }else if(80 > score >= 70){
+//         return "C"
+//     }else{
+//         return "D"
+//     }
+
+// }
+
+
+// const result = test(95);
+// console.log(result);
+// const a = document.querySelectorAll("h1");
+// console.log(a);
+
+// let a = 0;
+// let b = 3;
+// function test(){
+//     console.log(a, b)
+//     return a + b;
+// }
+
+// const c = test(1, 2);
+// console.log(c);
+// 3600 => 4200 4800
+// 6개월 1200
+
+
+// function slide(a, b){
+//     duration
+// }
+
+// slide(a, b)
